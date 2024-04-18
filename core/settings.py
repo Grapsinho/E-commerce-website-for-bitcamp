75474854ac242d0d72cddac513974d24e0160a0d
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-04-18 13:19:43 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://e-commerce-website-for-bitcamp.onrender.com"]
 
 # ესენი არის ქროსს საიტ სკრიპტინგისთვის, დაცვისთვის
 SECURE_BROWSER_XSS_FILTER = True
