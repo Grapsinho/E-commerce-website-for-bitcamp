b4e4d63faaf9536fc2680f723e645d5159710098
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-04-18 14:24:07 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # ყველა არა https რექვესტები გადამისამართდება httpზე
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
