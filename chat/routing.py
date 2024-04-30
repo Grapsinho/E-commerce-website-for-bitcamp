9458f254c819f188cb84572ed0c78439577ca550
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-04-30 11:06:32 -0700

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<chat_pk>\d+)/$', ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<chat_pk>\d+)/$', ChatConsumer.as_asgi()),
 ]
