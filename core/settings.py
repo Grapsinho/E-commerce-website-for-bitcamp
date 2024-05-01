75e685974a6bd773b39abb7f51bb5da0414ee479
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-05-01 03:13:15 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ecommercewebsite-q6y0.onrender.com", "localhost"]
 
